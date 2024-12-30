8bfcf6a4f8981a54909268d93d7c494932784341
Toxic Lmao <dhruvjangid077@gmail.com>
2024-12-30 23:09:39 +0530

    Proper login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,21 +1,74 @@
+"use client";
+
 import Image from "next/image";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import login2 from "@/public/images/login2.jpg";
+import { useState } from "react";
 
 export default function Login() {
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+    login: true,
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
   return (
     <div className="grid grid-cols-2">
       <div className="flex flex-col items-center justify-center gap-8 w-1/2 place-self-center text-nowrap">
-        <h1 className="text-3xl font-bold">Sign In to BlogLust</h1>
         <form className="flex flex-col items-center justify-center gap-6 w-full text-gray-200">
+          <div className="flex items-center justify-center gap-1 p-2 border border-gray-500 rounded-lg">
+            <input
+              type="radio"
+              id="login"
+              name="login"
+              className="hidden w-4 h-4"
+              checked={user.login}
+              onChange={() => setUser({ ...user, login: true })}
+              required
+            />
+            <label
+              htmlFor="login"
+              className={`text-lg font-medium px-3 py-1.5 rounded-md cursor-pointer ${
+                user.login && "bg-gray-500"
+              }`}
+            >
+              Login
+            </label>
+            <input
+              type="radio"
+              id="register"
+              name="login"
+              className="hidden w-4 h-4"
+              checked={!user.login}
+              onChange={() => setUser({ ...user, login: false })}
+              required
+            />
+            <label
+              htmlFor="register"
+              className={`text-lg font-medium px-3 py-1.5 rounded-md cursor-pointer ${
+                !user.login && "bg-gray-500"
+              }`}
+            >
+              Register
+            </label>
+          </div>
+
           <div className="flex flex-col gap-2 w-full">
             <label htmlFor="username" className="text-lg font-medium">
-              Username
+              {user.login ? "Username" : "Email"}
             </label>
             <input
               type="text"
               className="w-full py-1.5 px-3 border bg-transparent border-gray-500 rounded-md focus:-outline-offset-2"
+              name="username"
+              value={user.username}
+              onChange={handleChange}
+              required
             />
           </div>
           <div className="flex flex-col gap-2 w-full">
@@ -25,18 +78,22 @@ export default function Login() {
             <input
               type="password"
               className="w-full p-1.5 px-3 border bg-transparent border-gray-500 rounded-md focus:-outline-offset-2"
+              name="password"
+              value={user.password}
+              onChange={handleChange}
+              required
             />
           </div>
           <button
             type="submit"
             className="bg-[#EEEEEE] text-[#0f0f0f] text-lg font-semibold w-full px-3 py-1.5 rounded-md hover:bg-[#EEEEEE]/80 transition-all duration-300"
           >
-            Login
+            {user.login ? "Login" : "Register"}
           </button>
-          <div className="grid grid-cols-4 place-items-center w-full">
-            <hr className="w-full border-gray-500" />
-            <h3 className="col-span-2 text-lg font-medium">Or continue with</h3>
-            <hr className="w-full border-gray-500" />
+          <div className="grid grid-cols-5 place-items-center w-full">
+            <hr className="col-span-2 w-full border-gray-500" />
+            <h3 className="text-lg font-medium">or</h3>
+            <hr className="col-span-2 w-full border-gray-500" />
           </div>
           <div className="flex items-center justify-center gap-2 w-full">
             <button className="flex items-center justify-center gap-2 bg-[#EEEEEE] text-[#0f0f0f] text-lg font-semibold w-full px-3 py-1.5 rounded-md hover:bg-[#EEEEEE]/80 transition-all duration-300">
