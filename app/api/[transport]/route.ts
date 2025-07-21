bfa9333a2610a9b8d675d7ef80f1416bb73c9f0a
Dhruv Jangid <143801586+dhruv-jangid@users.noreply.github.com>
2025-07-21 22:49:10 +0530

    - Added @modelcontextprotocol/sdk version 1.16.0 - Added @vercel/mcp-adapter version 1.0.0 - Added redis version 5.6.0

---
+++ app/api/[transport]/route.ts
@@ -0,0 +1,30 @@
+import "server-only";
+import { getUserInfoTool } from "./tools";
+import { usernameValidator } from "@/lib/schemas/mcp";
+import { createMcpHandler } from "@vercel/mcp-adapter";
+
+const handler = createMcpHandler(
+  (server) => {
+    // @ts-expect-error - TS inference too deep in MCPServer tool
+    server.tool(
+      "getUserInfo",
+      "Give user's info based on username",
+      {
+        username: usernameValidator,
+      },
+      getUserInfoTool
+    );
+  },
+  {
+    capabilities: {
+      tools: {
+        getUserInfo: {
+          description: "Give user's info based on username",
+        },
+      },
+    },
+  },
+  { basePath: "/api", verboseLogs: true }
+);
+
+export { handler as GET, handler as POST, handler as DELETE };
